8827a6119075857518ea62dda16ece368e814697
utkir1 <otkirsoatboyev1@gmail.com>
2022-04-09 13:01:45 +0500

    initial commit

---
+++ js/script.js
@@ -0,0 +1,14 @@
+var minAge = 16;
+var userAge = prompt("Yoshingizni kiriting:");
+var difAge = minAge - userAge;
+var paragraph = document.createElement("h1");
+document.body.append(paragraph);
+
+if(userAge >= minAge){
+  console.log("Motosikl poygasiga muvaffaqqiyatli ro'yxatdan o'tdingiz");
+  paragraph.append("Motosikl poygasiga muvaffaqqiyatli ro'yxatdan o'tdingiz");
+}else
+{
+  console.log(` Yoshingiz to'g'ri kelmas ekan. \n ${difAge} yildan keyin urinib ko'ring `);
+  paragraph.append(` Yoshingiz to'g'ri kelmas ekan. \n ${difAge} yildan keyin urinib ko'ring`);
+}
