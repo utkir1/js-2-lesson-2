f0070eb910c8ad95dcbef4b0d3cac29ae59daa39
utkir1 <otkirsoatboyev1@gmail.com>
2022-04-12 09:02:48 +0500

    edited title

--- js/script.js
+++ js/script.js
@@ -3,7 +3,7 @@ var userAge = prompt("Yoshingizni kiriting:");
 var difAge = minAge - userAge;
 var paragraph = document.createElement("h1");
 document.body.append(paragraph);
-
+ 
 if(userAge >= minAge){
   console.log("Motosikl poygasiga muvaffaqqiyatli ro'yxatdan o'tdingiz");
   paragraph.append("Motosikl poygasiga muvaffaqqiyatli ro'yxatdan o'tdingiz");
